948b7a01c24d32593722b98b7e48c93b591de572
Santiago Benenatti <sbenenatti@gmail.com>
2024-11-14 19:38:19 -0300

    Cambios para vercel 13 (cambios de vistas)

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -21,10 +21,10 @@ exports.editDoctor = async (req, res) => {
     const { id } = req.params;
     try {
         const [doctor] = await db.promise().query('SELECT * FROM doctors WHERE id = ?', [id]);
-        if (doctor.length === 0) return res.status(404).send("Doctor not found");
+        if (doctor.length === 0) return res.status(404).send("Doctor no funciono");
         res.render('editDoctor', { doctor: doctor[0] });
     } catch (error) {
-        res.status(500).send("Error retrieving doctor data");
+        res.status(500).send("Error al recuperar datos del médico");
     }
 };
 
@@ -37,7 +37,7 @@ exports.updateDoctor = async (req, res) => {
         res.redirect('/doctors');
     } catch (error) {
         console.error(error);  
-        res.status(500).send("Error updating doctor");
+        res.status(500).send("Error al actualizar doctor");
     }
 };
 
